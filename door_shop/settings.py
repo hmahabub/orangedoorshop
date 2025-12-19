2bb59b55055e606b60ed4783169cba0933edf369
hmahabub <hm.sayem.14@gmail.com>
2025-12-19 10:17:25 +0600

    test commit

--- door_shop/settings.py
+++ door_shop/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0vb4z5e+6$rg-d_3f$rv4p_9ndos23n)u*x!nf-^!3fm5t8veb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',
-        'orangedoorshop.com',
-    'www.orangedoorshop.com',
+ALLOWED_HOSTS = [
 ]
 
 
